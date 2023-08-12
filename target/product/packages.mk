5c45d5c40c3959df3313e135fe6f57e67ba15412
Aaron Kling <webgeek1234@gmail.com>
2023-08-12 07:24:59 +0000

    config: Bring back ntfs tools
    +Exclude ntfs-3g from artifact path
    Change-Id: I408d533811e9b14a985b86ce3bf656cde5a37c98
    
    Change-Id: I1d3ac2d6684a0b2a7420c0a63e6e78ec0d55265b

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -30,6 +30,19 @@ PRODUCT_PACKAGES += \
     fsck.exfat \
     mkfs.exfat
 
+# Filesystems tools
+PRODUCT_PACKAGES += \
+    fsck.ntfs \
+    mkfs.ntfs \
+    mount.ntfs
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/fsck.ntfs \
+    system/bin/mkfs.ntfs \
+    system/bin/mount.ntfs \
+    system/%/libfuse-lite.so \
+    system/%/libntfs-3g.so
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
